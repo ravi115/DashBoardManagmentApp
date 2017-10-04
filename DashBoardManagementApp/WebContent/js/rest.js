dc007593141a5db098c455f12e944c0c251feaaf
RAVI RANJAN KUMAR <ravi115ranjan@gmail.com>
2017-10-04 11:12:57 +0530

    search function

--- DashBoardManagementApp/WebContent/js/rest.js
+++ DashBoardManagementApp/WebContent/js/rest.js
@@ -176,3 +176,34 @@ function invokeAPI(serverUrl, method, contentType, data, dataType) {
 	});
 	return responseData;
 }
+
+function doSearch() {
+	console.log("search function called on keypress!");
+    var searchText = document.getElementById('searchTerm').value.toUpperCase();
+    var targetTable = document.getElementById('itemList');
+    var targetTableColCount;
+            
+    //Loop through table rows
+    for (var rowIndex = 0; rowIndex < targetTable.rows.length; rowIndex++) {
+        var rowData = '';
+
+        //Get column count from header row
+        if (rowIndex == 0) {
+           targetTableColCount = targetTable.rows.item(rowIndex).cells.length;
+           continue; //do not execute further code for header row.
+        }
+                
+        //Process data rows. (rowIndex >= 1)
+        for (var colIndex = 0; colIndex < targetTableColCount; colIndex++) {
+            rowData += targetTable.rows.item(rowIndex).cells.item(colIndex).innerText.toUpperCase();
+        }
+
+        //If search term is not found in row data
+        //then hide the row, else show
+        if (rowData.indexOf(searchText) == -1)
+            targetTable.rows.item(rowIndex).style.display = 'none';
+        else
+            targetTable.rows.item(rowIndex).style.display = 'table-row';
+    }
+}
+
